c51c24eb9a0772bb47c9dbb2cc4e4d1229fa60f4
rosanaemidio <rosanaemidio42@gmail.com>
2022-10-15 10:41:13 -0300

    fix: removido parametro id

--- exercicios/para-sala/exercicio-2/resolucao/resolucao.js
+++ exercicios/para-sala/exercicio-2/resolucao/resolucao.js
@@ -1,4 +1,4 @@
-function Employee(id,firstname, lastname, salary) {
+function Employee(firstname, lastname, salary) {
     const employee = {}
     employee.id = "id" + Math.floor(Math.random() * 2000)
     employee.firstname = firstname
@@ -8,8 +8,8 @@ function Employee(id,firstname, lastname, salary) {
     return employee
 }
 
-const func1 = Employee("Id","Rosana", "Emidio", 10_000)
-const func2 = Employee("Id","Melissa", "Gomes", 2_000)
+const func1 = Employee("Rosana", "Emidio", 10_000)
+const func2 = Employee("Melissa", "Gomes", 2_000)
 
 console.log(func1)
 console.log(func2)
