cfe9394892e60f5aedb3b38a34011a84c02e71a8
rosanaemidio <rosanaemidio42@gmail.com>
2022-10-15 10:03:36 -0300

    adicionado metodo ao exercicio

--- exercicios/para-sala/exercicio-1/resolucao/resolucao.js
+++ exercicios/para-sala/exercicio-1/resolucao/resolucao.js
@@ -2,6 +2,12 @@ const employee = {}
 employee.id = 25;
 employee.firstname = 'Rosana';
 employee.lastname  = 'Emidio';
-employee.salary = '10000.00'
+employee.salary = 10_000;
 
-console.log(employee)
+employee.raiseSalary = function raiseSalary(percent) {
+    const newSalary = (this.salary * percent) + this.salary;
+    this.salary = newSalary;
+    return newSalary
+}
+
+console.log(employee.raiseSalary(0.20))
