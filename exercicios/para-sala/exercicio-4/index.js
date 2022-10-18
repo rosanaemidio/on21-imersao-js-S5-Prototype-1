e8f1ce960205ecaac5f8fcb089b6775394a16d36
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2022-10-18 19:58:49 -0300

    update exercise 4 response

--- exercicios/para-sala/exercicio-4/index.js
+++ exercicios/para-sala/exercicio-4/index.js
@@ -1,1 +1,54 @@
 //exercicios feitos pela profa aqui
+function generateId() {
+	const crypto = require('crypto');
+	const id = crypto.randomUUID();
+	return id;
+}
+
+const employeeMethods = {
+	raiseSalary: function raiseSalary(percent) {
+		this.salary += (this.salary * percent) / 100;
+		return `O salário com o aumento totaliza R$ ${this.salary}`;
+	},
+
+	addBenefits: function addBenefits(benefit) {
+		this.benefits.push(benefit);
+		console.log(`${benefit} adicionado aos benefícios`);
+	},
+
+	removeBenefits: function removeBenefits(benefit) {
+		this.benefits = this.benefits.filter(function (element) {
+			return element !== benefit;
+		});
+		console.log(`${benefit} removido dos benefícios`);
+	},
+
+	listBenefits: function listBenefits() {
+		this.benefits.forEach((benefit) => console.log(`- ${benefit}`));
+	},
+};
+
+function Employee(firstName, lastName, salary) {
+	let employee = {};
+
+	employee.id = generateId();
+	employee.firstName = firstName;
+	employee.lastName = lastName;
+	employee.salary = salary;
+	employee.benefits = [];
+
+	employee.raiseSalary = employeeMethods.raiseSalary;
+	employee.addBenefits = employeeMethods.addBenefits;
+	employee.removeBenefits = employeeMethods.removeBenefits;
+	employee.listBenefits = employeeMethods.listBenefits;
+
+	return employee;
+}
+
+const employee1 = Employee('Luara', 'Kerlen', 10000);
+employee1.addBenefits('VR');
+employee1.addBenefits('VA');
+employee1.addBenefits('Zenklub');
+employee1.listBenefits();
+employee1.removeBenefits('VA');
+employee1.listBenefits();
