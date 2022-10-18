c869af72170d275f523811999d818f2f55429314
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2022-10-18 19:37:09 -0300

    update file 4

--- material/4 - functionalShared.js
+++ material/4 - functionalShared.js
@@ -24,9 +24,19 @@ function Animal(type, name, age) {
 const animal1 = Animal('cachorro', 'Aslam', 3);
 const animal2 = Animal('gato', 'Caju', 1);
 
-console.log(animal1);
-console.log(animal2);
+animal1.eat()
+animal2.eat()
 
-console.log(animal2.energy);
-animal2.sleep(12);
-console.log(animal2.energy);
+// console.log(animal1);
+// console.log(animal2);
+
+// console.log(animal2.energy);
+// animal2.sleep(12);
+// console.log(animal2.energy);
+
+animalMethods.eat = function eat() {
+	console.log('Agora estou executando esse método')
+}
+
+const animal3 = Animal('leão', 'Alex', 6)
+animal3.eat()
