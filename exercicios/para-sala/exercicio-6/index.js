924989468de375f14728e6cea14499f8a9b8c908
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2022-10-18 21:35:43 -0300

    update exercise 6 response

--- exercicios/para-sala/exercicio-6/index.js
+++ exercicios/para-sala/exercicio-6/index.js
@@ -1,1 +1,40 @@
 //exercicios feitos pela profa aqui
+function Account(accountNumber, agencyNumber, amount) {
+	const account = Object.create(Account.prototype);
+
+	account.accountNumber = accountNumber;
+	account.agencyNumber = agencyNumber;
+	account.amount = amount;
+
+	return account;
+}
+
+Account.prototype.credit = function credit(amount) {
+	this.amount += amount;
+	console.log(`O saldo atual é de R$ ${this.amount}`);
+};
+
+Account.prototype.debit = function debit(amount) {
+	this.amount -= amount;
+	console.log(`O saldo atual é de R$ ${this.amount}`);
+};
+
+Account.prototype.transferTo = function transferTo(
+	anotherAccount,
+	valorASerTransferido
+) {
+	if (this.amount < valorASerTransferido) {
+		throw 'ERRO!!! Você não possui saldo suficiente para realizar essa operação';
+	}
+
+	this.debit(valorASerTransferido);
+	anotherAccount.credit(valorASerTransferido);
+};
+
+const contaDaLuara = Account(111, 222, 1000);
+const contaDaMaria = Account(444, 333, 600);
+
+// console.log(contaDaLuara)
+// console.log(contaDaMaria)
+
+contaDaLuara.transferTo(contaDaMaria, 300);
